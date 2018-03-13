31c36c194bf12cb6a64d9f9425fce7bf78accb4d
Michael Krotscheck <krotscheck@gmail.com>
2018-03-12 22:05:10 -0700

    feat(storage): rx-js storage library (#16)
    This library contains an rx/js abstraction on top of the browser's
    localStore and sessionStore.

---
+++ @kangaroo/rxjs-storage/src/local-storage.subject.ts
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2018 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Subject } from 'rxjs/Subject';
+import { ObjectUnsubscribedError } from 'rxjs/Rx'; // tslint:disable-line
+import { Subscriber } from 'rxjs/Subscriber';
+import { ISubscription, Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+
+/**
+ * This subject behaves much like a BehaviorSubject, however the value is
+ * backed by the browser's localStorage.
+ */
+export class LocalStorageSubject<T> extends Subject<T> {
+
+  /**
+   * Our hot subscription which keeps our subject alive.
+   */
+  private eventSubscription: Subscription;
+
+  /**
+   * Reference to the local storage instance.
+   */
+  private readonly storage: Storage;
+
+  /**
+   * Create a new behavior subject instance.
+   *
+   * @param _key The storage key.
+   */
+  constructor(private _key: string) {
+    super();
+
+    this.storage = window.localStorage;
+
+    // Listen to localstorage events, for changes in other browser windows.
+    // This event is then passed to super.next() to bypass our internal storage,
+    // as the value will already have been stored by another browser.
+    this.eventSubscription = Observable
+      .fromEvent(window, 'storage')
+      .filter((e: StorageEvent) => e.key === this._key)
+      .map(() => this.decodedValue())
+      .subscribe((value) => super.next(value));
+  }
+
+  /**
+   * Convenience accessor for the current value.
+   *
+   * @returns The current value.
+   */
+  get value(): T {
+    return this.getValue();
+  }
+
+  /**
+   * Finish usage of this subject.
+   */
+  public complete() {
+    super.complete();
+
+    if (this.eventSubscription && !this.eventSubscription.closed) {
+      this.eventSubscription.unsubscribe();
+    }
+    this.eventSubscription = null;
+  }
+
+  /**
+   * Unsubscribe this subject from its upstream components.
+   */
+  public unsubscribe() {
+    super.unsubscribe();
+
+    if (this.eventSubscription && !this.eventSubscription.closed) {
+      this.eventSubscription.unsubscribe();
+    }
+  }
+
+  /**
+   * Return the current value.
+   */
+  public getValue(): T {
+    if (this.hasError) {
+      throw this.thrownError;
+    } else if (this.closed) {
+      throw new ObjectUnsubscribedError();
+    } else {
+      return this.decodedValue();
+    }
+  }
+
+  /**
+   * Send the next value, persisting it to local storage.
+   *
+   * @param value The next value.
+   */
+  public next(value: T): void {
+    const v = JSON.stringify(value);
+    this.storage.setItem(this._key, v);
+    super.next(value);
+  }
+
+  /**
+   * When subscribed, begin by emitting the current value of the subject.
+   *
+   * @param subscriber The subscriber.
+   * @returns An active subscription.
+   */
+  protected _subscribe(subscriber: Subscriber<T>): Subscription {
+    const subscription = super._subscribe(subscriber);
+    if (subscription && !(<ISubscription>subscription).closed) {
+      subscriber.next(this.decodedValue());
+    }
+    return subscription;
+  }
+
+  /**
+   * Lifecycle-decoupled value decoder.
+   *
+   * @returns The type-decoded value from the local storage.
+   */
+  private decodedValue(): T {
+    const rawValue = this.storage.getItem(this._key);
+    if (rawValue === null) {
+      return undefined;
+    }
+    try {
+      return JSON.parse(rawValue);
+    } catch (e) {
+      return undefined;
+    }
+  }
+}
---
+++ @kangaroo/rxjs-storage/src/session-storage.subject.spec.ts
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2018 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { SessionStorageSubject } from './session-storage.subject';
+
+describe('SessionStorageSubject', () => {
+
+  const testValue = {
+    stringValue: 'string',
+    intValue: 100,
+    doubleValue: 1.234,
+    booleanValue: true
+  };
+  let s: SessionStorageSubject<any>;
+
+  beforeEach(() => {
+    window.sessionStorage.clear();
+    s = new SessionStorageSubject<any>('test_key');
+  });
+
+  afterEach(() => {
+    s.unsubscribe();
+    s = null;
+  });
+
+  it('should permit subscription to a key.', () => {
+    let recipient = null;
+
+    s.subscribe((value) => recipient = value);
+    s.next('Foo');
+    expect(recipient).toEqual('Foo');
+    s.next('Bar');
+    expect(recipient).toEqual('Bar');
+  });
+
+  it('should permit access via value and getValue', () => {
+    s.next(testValue);
+
+    expect(s.getValue()).toEqual(testValue);
+    expect(s.value).toEqual(testValue);
+  });
+
+  it('should throw an error on getValue if an error was reported', () => {
+    s.error('this is an error');
+    try {
+      s.getValue();
+      fail();
+    } catch (e) {
+      // pass
+    }
+  });
+
+  it('should throw an error on getValue if closed', () => {
+    s.unsubscribe();
+    try {
+      s.getValue();
+      fail();
+    } catch (e) {
+      // pass
+    }
+  });
+
+  it('should be type-safe', () => {
+    s.next(testValue);
+    s.subscribe((value) => expect(value).toEqual(testValue));
+  });
+
+  it('should broadcast the last value on subscription', () => {
+    s.next(testValue);
+    s.subscribe((value) => expect(value).toEqual(testValue));
+  });
+
+  it('should load values from existing storage', () => {
+    window.sessionStorage.setItem('test_key', JSON.stringify(testValue));
+    s.subscribe((value) => expect(value).toEqual(testValue));
+  });
+
+  it('should return undefined on empty', () => {
+    s.subscribe((value) => expect(value).toBeUndefined());
+  });
+
+  it('should return undefined on parse error', () => {
+    window.sessionStorage.setItem('test_key', JSON.stringify(testValue).slice(0, 10));
+    s.subscribe((value) => expect(value).toBeUndefined());
+  });
+
+  it('should not emit a value if the subject is complete.', () => {
+    s.complete();
+    s.subscribe(() => fail());
+  });
+
+  it('should throw an error if an error has been reported.', () => {
+    s.error('This is an error');
+    try {
+      s.subscribe(() => fail());
+      fail();
+    } catch (e) {
+      // pass
+    }
+  });
+});
